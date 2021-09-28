a228497a121626798e2c4881422897eaef2f7d5a
Ilya Smirnov <ismirnov@aligntech.com>
2021-09-28 16:07:44 +0300

    fix: get locale instead

--- aiogram_dialog/widgets/kbd/calendar_kbd.py
+++ aiogram_dialog/widgets/kbd/calendar_kbd.py
@@ -31,7 +31,7 @@ PREFIX_YEAR = "YEAR"
 
 MONTHS_NUMBERS = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (10, 11, 12)]
 
-if locale.getlocale(locale.LC_ALL) == 'RU_ru':
+if locale.getlocale(locale.LC_TIME)[0] == 'ru_RU':
     DAYNAMES = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
 else:
     DAYNAMES = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
